c61e7c6a58b146862e8d16d5f7450143ceae8675
Alexander Simpson <as2388@cam.ac.uk>
2016-02-25 21:29:26 +0000

    Enable color from list

--- extension.js
+++ extension.js
@@ -10,6 +10,10 @@
         return {status: 2, msg: 'Ready'};
     };
 
+    ext.updatePiAddress = function(newAddress) {
+        apiRoot = "http://" + newAddress + "/api/v1/";
+    }
+
     function sendRequest(path, callback) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", apiRoot + path);
@@ -19,9 +23,28 @@
         xhr.send(null);
     }
 
+    function sendRequest(path) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", apiRoot + path);
+        xhr.send(null);
+    }
+
     ext.sendMessage = function(message, callback) {
         sendRequest("sendMessage/" + message, callback);
     };
+
+    ext.switchOnLed = function(x, y, r, g, b) {
+        sendRequest("switchOnLed/" + x + "/" + y + "/" + r + "/" + g + "/" + b);
+    };
+
+    ext.switchOnLedWithColor = function(x, y, color) {
+        var r, g, b;
+        switch(color) {
+            case "white": r = 255; g = 255; b = 255; break;
+            case "black": r = 0; g = 0; b = 0; break;
+        }
+        ext.switchOnLed(x, y, r, g, b);
+    }
 
     ext.setLowLight = function(lowLight, callback) {
         var xhr = new XMLHttpRequest();
@@ -61,12 +84,6 @@
         xhr.send(null);
     };
 
-    ext.switchOnLed = function(x, y, r, g, b) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", apiRoot + "switchOnLed/" + x + "/" + y + "/" + r + "/" + g + "/" + b);
-        xhr.send(null);
-    };
-
     ext.switchOffLed = function(x, y) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", apiRoot + "switchOffLed/" + x + "/" + y);
@@ -82,21 +99,21 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            //['w', 'set Raspberry Pi address to %s', '192.168.3.2:80']
+            ['w', 'set Raspberry Pi address to %s', 'updatePiAddress', '192.168.3.2:80']
             //[' ', 'set LED matrix rotation to %m.udlr', 'up'],
-            ['w', 'show message %s', 'sendMessage', 'Hello, World!'],
+            [' ', 'show message %s', 'sendMessage', 'Hello, World!'],
             /*[' ', 'show letter %s', 'A'],
-            [' ', 'turn low light mode %m.onoff', 'setLowLight', 'on'],
-            [' ', 'set LED x %n y %n to color %m.color', 'white']
+            [' ', 'turn low light mode %m.onoff', 'setLowLight', 'on'],*/
+            [' ', 'set LED x %n y %n to color %m.color', 'switchOnLedWithColor' 'white']
             [' ', 'set LED x %n y %n to color red %n green %n blue %n', 'switchOnLed', 0, 0, 255, 255, 255],
-            ['R', '%m.rgb component of LED x %n y%n', 'red', 0, 0],
+            /*['R', '%m.rgb component of LED x %n y%n', 'red', 0, 0],
             ['R', 'color of LED x %n y %n', 0, 0,],
             [' ', 'switch off LED x %n y %n', 'switchOffLed', 0, 0],
             [' ', 'clear LEDs', 'clear'],
             ['R', 'temperature', 'getTemperature'],
             ['R', 'relative humidity'],
             ['R', 'pressure'],*/
-            ['R', 'direction']
+            //['R', 'direction']
             /*['R', 'orientation %m.pyr', 'getOrientation', 'pitch'],
             ['R', 'raw accelerometer %m.xyz', 'getAccelRaw', 'x']*/
         ],
