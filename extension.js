cb3bc0ba236ed5dcd4c62f4cc586415ed073ca87
Alexander Simpson <as2388@cam.ac.uk>
2016-02-25 21:39:29 +0000

    update

--- extension.js
+++ extension.js
@@ -100,12 +100,12 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['w', 'set Raspberry Pi address to %s', 'updatePiAddress', '192.168.3.2:80']
+            ['w', 'set Raspberry Pi address to %s', 'updatePiAddress', '192.168.3.2:80'],
             //[' ', 'set LED matrix rotation to %m.udlr', 'up'],
             [' ', 'show message %s', 'sendMessage', 'Hello, World!']
             /*[' ', 'show letter %s', 'A'],
             [' ', 'turn low light mode %m.onoff', 'setLowLight', 'on'],*/
-            //[' ', 'set LED x %n y %n to color %m.color', 'switchOnLedWithColor', 0, 0, 'white']
+            [' ', 'set LED x %n y %n to color %m.color', 'switchOnLedWithColor', 0, 0, 'white']
             //[' ', 'set LED x %n y %n to color red %n green %n blue %n', 'switchOnLed', 0, 0, 255, 255, 255],
             /*['R', '%m.rgb component of LED x %n y%n', 'red', 0, 0],
             ['R', 'color of LED x %n y %n', 0, 0,],
